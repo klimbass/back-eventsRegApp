be630d7cf355e343ed421537a3f394cb6755e714
Alex <112724167+klimbass@users.noreply.github.com>
2024-09-24 00:51:09 +0300

    check cors set

--- src/server.js
+++ src/server.js
@@ -8,31 +8,32 @@ import router from './routers/index.js';
 
 const PORT = Number(env('PORT', 3000));
 
-const allowedOrigins = [
-  env('DEPLOYEDFRONT', ' '),
-  env('LOCALHOST'),
-  'http://localhost:3000',
-];
+// const allowedOrigins = [
+//   env('DEPLOYEDFRONT', ' '),
+//   env('LOCALHOST'),
+//   'http://localhost:3000',
+// ];
 
 export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
 
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        console.error(`Blocked by CORS: ${origin}`);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200,
-  };
+  // const corsOptions = {
+  //   origin: (origin, callback) => {
+  //     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+  //       callback(null, true);
+  //     } else {
+  //       console.error(`Blocked by CORS: ${origin}`);
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   credentials: true,
+  //   optionsSuccessStatus: 200,
+  // };
 
-  app.use(cors(corsOptions));
+  // app.use(cors(corsOptions));
+  app.use(cors());
 
   app.use(
     pino({
